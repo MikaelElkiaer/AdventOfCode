416564dbfad5c904d64e92aa2d516a1f32c5a1e5
MikaelElkiaer <mikaelec@gmail.com>
2018-12-13 22:32:55 +0100

    Implemented Day 1, with example unit tests.

---
+++ src/AdventOfCode.Day1.Tests/Tests.fs
@@ -0,0 +1,34 @@
+module AdventOfCode.Day1.Tests
+
+open System
+open System.IO
+open Xunit
+open AdventOfCode.Day1
+
+[<Fact>]
+let ``Day1 - Example 1`` () =
+    let expected = 3
+    let actual =
+        adjust 0 [1;-2;3;1]
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``Day1 - Example 2`` () =
+    let expected = 3
+    let actual =
+        adjust 0 [1;1;1]
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``Day1 - Example 3`` () =
+    let expected = 0
+    let actual =
+        adjust 0 [1;1;-2]
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``Day1 - Example 4`` () =
+    let expected = -6
+    let actual =
+        adjust 0 [-1;-2;-3]
+    Assert.Equal(expected, actual)
---
+++ src/AdventOfCode.Day1/Day1.fs
@@ -0,0 +1,14 @@
+﻿namespace AdventOfCode
+
+open System
+
+module Day1 =
+    let rec adjust frequency changes =
+        match changes with
+        | head :: tail -> adjust (frequency + head) tail
+        | [] -> frequency
+
+    let parseFile filePath =
+        System.IO.File.ReadAllLines filePath
+        |> Array.toList
+        |> List.map Int32.Parse
